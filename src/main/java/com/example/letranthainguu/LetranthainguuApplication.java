8e81e91eb9c59d09b9d852beb56bbae8d95b4d5e
LeTranThaiNguu <thainguu3007@gmail.com>
2025-02-24 10:39:19 +0700

    web2 project

---
+++ src/main/java/com/example/letranthainguu/LetranthainguuApplication.java
@@ -0,0 +1,13 @@
+package com.example.letranthainguu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetranthainguuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetranthainguuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/letranthainguu/LetranthainguuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.letranthainguu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetranthainguuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
