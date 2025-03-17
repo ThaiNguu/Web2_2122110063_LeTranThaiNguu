7bf0e4105f4b846e37aaf1cab2155314f98d2f1c
LeTranThaiNguu <thainguu3007@gmail.com>
2025-03-17 10:49:50 +0700

    update-17-03-2025

---
+++ src/main/java/com/example/letranthainguu/backend/domain/Banner.java
@@ -0,0 +1,34 @@
+package com.example.letranthainguu.backend.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "banners")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Banner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String image;
+
+    private String link;
+
+    private Integer status;
+
+    private String position;
+}
